c9ca1c32655a40a25982fde9a10d5d9457d3ddeb
bhattaraimadhu56 <bhattaraimadhu56@gmail.com>
2020-10-30 04:04:13 +1300

    Initial Commit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+// import Home from "./screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    <View style={container}>
+      <Text> 1. Hello from App.js </Text>
       <StatusBar style="auto" />
+      {/* <Home /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     // type colorpicker in google and select your prefer color
+//     backgroundColor: "#7ecc98",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+// OR Simply do like this
+
+const container = {
+  flex: 1,
+  // type colorpicker in google and select your prefer color
+  backgroundColor: "#7ecfff",
+  alignItems: "center",
+  justifyContent: "center",
+};
